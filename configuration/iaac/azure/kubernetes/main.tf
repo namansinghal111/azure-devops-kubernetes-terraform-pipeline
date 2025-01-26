6c9647aca49a3ce12aaf821511ab4ce98738237e
namansinghal <72150803+namansinghal111@users.noreply.github.com>
2025-01-26 18:04:26 +0530

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -48,8 +48,8 @@ required_providers {
   }
   backend "azurerm" {
     storage_account_name="storageaccountnaman5" #OVERRIDE in TERRAFORM init
-    access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-    key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
+   # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
+   # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
     container_name="storageaccountnamancontainer" #OVERRIDE in TERRAFORM init
   }
 }
